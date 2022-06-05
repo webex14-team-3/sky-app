d1654d3dff72546159f15bf6f91988d02d512af3
kujikazuaki <ajmjmjmdpjwtaj@gmail.com>
2022-06-05 21:29:37 +0900

    ログイン設定

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <template>
-  <div>
-    <h1>test</h1>
-    <Login />
-  </div>
   <nav>
-    <router-link to="/">HomeView</router-link> |
-    <router-link to="/Login">Login</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </nav>
+  <div>
+    <NavBar />
+  </div>
   <router-view />
-  <v-app>
-    <router-view />
-  </v-app>
 </template>
 
-<script>
-import Login from "./views/Login.vue"
-export default {
-  views: { Login },
-  // components: { Login },
-}
-</script>
-
 <style>
-#app {
+
+
+<script>
+import NavBar from "@/components/NavBar.vue"
+export default {
+  components: {
+    NavBar,
+  },
+}
+
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+body {
+  margin: 0;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
