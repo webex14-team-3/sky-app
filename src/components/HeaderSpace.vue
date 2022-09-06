98f2cc92fcc59421b72ac7928730c7a9bd65ffac
kujiKazuaki <KujiKazuaki24@gmail.com>
2022-09-06 20:38:19 +0900

    header/css//imporove

--- src/components/HeaderSpace.vue
+++ src/components/HeaderSpace.vue
@@ -1,50 +1,29 @@
 <template>
-  <header class="container">
-    <div class="headerleft">
-      <!-- ナビゲーションパート start -->
-      <nav class="hamburger-menu">
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn">
-          <div class="menu-icon-wide">
-            <span class="nav-icon"></span>
-          </div>
-        </label>
-        <!-- 見出し -->
-        <ul class="header-menu" v-on:click="liButton">
-          <li class="header-munu-one">
-            <router-link to="/">
-              <span class="menu-text" id="menu-text-one"
-                >トップページ</span
-              ></router-link
-            >
-          </li>
-          <li>
-            <router-link to="myAccount">
-              <span class="menu-text" id="menu-text-two"
-                >マイページ</span
-              ></router-link
-            >
-          </li>
-          <li>
-            <router-link to="aboutView">
-              <span class="menu-text" id="menu-text-three"
-                >プロフィール設定</span
-              ></router-link
-            >
-          </li>
-        </ul>
-      </nav>
-      <!-- ナビゲーションパート end -->
-
-      <!-- テキストパート start -->
-      <div class="header-title">GSコネクト</div>
-      <!-- テキストパート end -->
+  <div class="allContainer">
+    <div class="baseContainer">
+      <router-link to="/" class="navLogo navLink" style="text-decoration: none"
+        ><a class="container">トップページ</a></router-link
+      >
+      <div class="navItems">
+        <router-link
+          to="/myAccount"
+          class="navItem navLink"
+          style="text-decoration: none"
+          ><a class="container"> マイページ</a></router-link
+        >
+        <router-link
+          to="/aboutView"
+          class="navItem navLink"
+          style="text-decoration: none"
+          ><a class="container">プロフィール<br />設定</a></router-link
+        >
+      </div>
+      <button class="loginButton" @click="googleLogin">
+        <a class="container" v-if="loginName">Login</a>
+        <a class="container" v-else>Logout</a>
+      </button>
     </div>
-
-    <!-- ログインパート start -->
-    <button class="login-button" @click="googleLogin"><a>ログイン</a></button>
-    <!-- ログインパート end -->
-  </header>
+  </div>
 </template>
 
 <script>
@@ -53,202 +32,125 @@ import { doc, setDoc } from "firebase/firestore"
 import { db } from "@/firebase"
 
 export default {
+  data() {
+    return {
+      loginName: true,
+    }
+  },
   methods: {
     googleLogin() {
-      // new"ネームプレート" provider "インスタンス"
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth()
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          console.log(result.user)
-          this.$store.commit("updateUserInfo", { uid: result.user.uid })
-          this.$store.commit("updateUserName", {
-            name: result.user.displayName,
+      if (this.loginName === true) {
+        // new"ネームプレート" provider "インスタンス"
+        const provider = new GoogleAuthProvider()
+        const auth = getAuth()
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            console.log(result.user)
+            this.$store.commit("updateUserInfo", { uid: result.user.uid })
+            this.$store.commit("updateUserName", {
+              name: result.user.displayName,
+            })
+            this.$store.commit("updateUserImage", {
+              image: result.user.photoURL,
+            })
+            console.log(this.$store.state.user.uid)
+            console.log(this.$store.state.name.name)
+            console.log(this.$store.state.image.image)
+            return result.user.uid
           })
-          this.$store.commit("updateUserImage", { image: result.user.photoURL })
-          console.log(this.$store.state.user.uid)
-          console.log(this.$store.state.name.name)
-          console.log(this.$store.state.image.image)
-          return result.user.uid
-        })
-        .then(async (uid) => {
-          // {}入れないとデータベースに入る形での登録はできない
-          // ※今まではauthの認証で入ってただけ
-          await setDoc(doc(db, "users", `${uid}`), {}, { merge: true })
-        })
-      // this.$router.push({
-      //   path: "/aboutView",
-      // })
+          .then(async (uid) => {
+            // {}入れないとデータベースに入る形での登録はできない
+            // ※今まではauthの認証で入ってただけ
+            await setDoc(doc(db, "users", `${uid}`), {}, { merge: true })
+            this.loginName = false
+          })
+        // this.$router.push({
+        //   path: "/aboutView",
+        // })
+      } else {
+        this.loginName = true
+      }
     },
   },
 }
 </script>
 
 <style scoped>
-.container {
+.allContainer {
+  /* border: 2px solid red; */
+  height: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
   background-color: #c7887fdd;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   margin: -9px;
 }
-.headerleft {
+.baseContainer {
+  /* border: 2px solid blue; */
+  width: 100%;
   display: flex;
+  justify-content: flex-end;
 }
-
-/* ナビゲーションパート start */
-
-.hamburger-menu {
-  /* border: 2px solid red; */
-
-  margin-right: 8px;
-}
-/* メニューアイコン（三本線）の真ん中の線です */
-.hamburger-menu .menu-icon .nav-icon {
-  background: #ffc107; /* 色は自由に変更可能です */
-  display: block;
-  height: 8px; /* 太さ */
-  position: relative;
-  top: 20px;
-  -webkit-transition: background 0.4s ease-out;
-  -o-transition: background 0.4s ease-out;
-  /* 形が変わる時のアニメーション */
-  transition: background 0.4 ease-out;
-  margin: 12px 0 0 12px;
-  z-index: 20;
-}
-.menu-icon-wide {
-  /* border: 2px solid red; */
-  height: 70px;
-  width: 80px;
-  padding-right: 10px;
-  position: fixed;
-  z-index: 11;
-}
-/* メニューアイコン（三本線）の上と下の線を疑似要素で追加 */
-.hamburger-menu .menu-icon .nav-icon::before,
-.hamburger-menu .menu-icon .nav-icon::after {
-  background: #ffc107; /* 色は自由に変更可能です */
-  content: "";
-  display: block;
-  height: 100%;
-  position: absolute;
-  -webkit-\aboutviewtransition: all 0.4s ease-out;
-  -o-transition: all 0.4s ease-out;
-  /* 形が変わる時のアニメーション */
-  transition: all 0.4s ease-out;
-  width: 100%;
-  z-index: 20;
-}
-.hamburger-menu .menu-icon .nav-icon::before {
-  top: -20px;
-} /* 位置を上にずらしています */
-.hamburger-menu .menu-icon .nav-icon::after {
-  top: 20px;
-} /* 位置を下にずらしています */
-/* 表示されるメニューです */
-.header-menu {
-  z-index: 10;
-  padding: 0;
-  background-color: rgba(255, 255, 255, 0.9);
-  overflow: hidden;
-  max-height: 0; /* ★最初は高さを0にして非表示状態に */
-  -webkit-transition: max-height 0.6s;
-  -o-transition: max-height 0.6s;
-  transition: max-height 0.6s;
-  text-align: center;
-  list-style: none;
-  width: 100%;
-  height: 100vh;
-  position: fixed;
-}
-.header-menu span {
-  user-select: none;
-}
-/* メニュー部分のデザインです */
-
-.hamburger-menu .header-menu li a {
-  display: block;
-  text-decoration: none;
-  text-transform: uppercase;
-  margin: 50px 0 0 0;
-  padding: 40px;
-  /* border: 2px solid red; */
-}
-.hamburger-menu .header-menu li a:hover {
-  background-color: #f1f1f1b1;
-  filter: brightness(90%);
-}
-/* .hamburger-menu .header-menu li:active {
-} */
-/* チェックボックスは常に非表示です */
-.hamburger-menu .menu-btn {
-  display: none;
-}
-/* ▼▼▼以下はチェックボックスがONの時の状態です▼▼▼ */
-.hamburger-menu .menu-btn:checked ~ .header-menu {
-  max-height: 100vh;
-  transition: max-height 0.6s;
-}
-/* メニューボタンの中央の線を非表示に */
-.hamburger-menu .menu-btn:checked ~ .menu-icon .nav-icon {
-  background: transparent;
-  transition: all 300ms linear 0ms;
-}
-
-/* メニューボタンの上下の線を45度傾けて✕印を作ります */
-.hamburger-menu .menu-btn:checked ~ .menu-icon .nav-icon::before {
-  transform: rotate(-45deg);
-  top: 30px;
-}
-.hamburger-menu .menu-btn:checked ~ .menu-icon .nav-icon::after {
-  transform: rotate(45deg);
-  top: 30px;
-}
-.menu-text {
-  background-image: linear-gradient(135deg, #fccb90 0%, #d57eeb 100%);
-  -webkit-background-clip: text;
-  -moz-background-clip: text;
-  background-clip: text;
-  color: transparent;
-  font-size: 30px;
-  font-weight: bold;
-  border-bottom: 2px solid #d57eeb;
-  /* border: 2px solid red; */
-}
-.header-munu-one {
-  /* border: 2px solid red; */
-  margin-top: 90px;
-}
-.hamburger-menu-login:hover {
-  cursor: pointer;
-}
-
-/* ナビゲーションパート end */
-
-/* テキストパート start */
-.header-title {
-  font-size: 50px;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  padding: 7px 90px 0;
-  user-select: none;
-  font-weight: 900;
-}
-/* テキストパート end */
-
-/* ログインパート start */
-.login-button {
-  border: 2px solid #c7887fdd;
+.navLogo {
+  /* border: 2px solid yellow; */
   background-color: rgba(255, 235, 205, 0.747);
+  width: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+.topPage {
   color: black;
   font-weight: bold;
   font-size: 20px;
-}
-.login-button a {
   user-select: none;
 }
-.login-button:hover {
+.navItems {
+  /* border: 2px solid black; */
+  display: flex;
+  align-items: center;
+  min-width: 40%;
+}
+.navItem {
+  /* border: 2px solid black; */
+  width: 100%;
+  background-color: rgba(255, 235, 205, 0.747);
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+.container {
+  /* border: 2px solid red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  font-weight: bold;
+  font-size: 20px;
+  user-select: none;
+  padding: 0px 5px;
+  text-decoration-line: none;
+}
+.navLink {
+  /* border: 2px solid green; */
+  height: 100%;
+  border-right: 5px solid #df5f5f;
+}
+.navLink:hover {
   cursor: pointer;
   background-color: #fccb90;
 }
-/* ログインパート end */
+.loginButton {
+  border: 2px solid #c7887fdd;
+  background-color: rgba(255, 235, 205, 0.747);
+  width: 20%;
+}
+.loginButton:hover {
+  cursor: pointer;
+  background-color: #fccb90;
+}
 </style>
