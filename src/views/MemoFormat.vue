d94645a8c567d477acdf590c6b4e86b62a0e8544
kujiKazuaki <KujiKazuaki24@gmail.com>
2022-09-27 23:00:54 +0900

    改変

--- src/views/MemoFormat.vue
+++ src/views/MemoFormat.vue
@@ -122,10 +122,6 @@ export default {
   },
   methods: {
     async postMemo() {
-      let remainTitle = []
-      let remainMemo = []
-      remainTitle.push(this.inputTitle)
-      remainMemo.push(this.inputMemo)
       // タイトルとテキストの両方が書かれていないと投稿できないようにする
       if (this.inputTitle !== "" && this.inputMemo !== "") {
         if (window.confirm("これで投稿しますか？")) {
@@ -190,9 +186,13 @@ export default {
           this.inputMemo = ""
         }
       } else {
-        alert("どっちも書いてください！")
+        let remainTitle = []
+        let remainMemo = []
+        remainTitle.push(this.inputTitle)
+        remainMemo.push(this.inputMemo)
         this.inputTitle = remainTitle
         this.inputMemo = remainMemo
+        alert("どっちも書いてください！")
       }
     },
     async saveMemo() {
